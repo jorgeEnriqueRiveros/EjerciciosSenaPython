301955b30c10c4e0aa5c1605936c7032397f46f2
K1K3R1V3R05T3CS0FT <k1k3r1v3r052029@gmail.com>
2024-11-12 19:48:02 -0500

    haciendo ejercicios de practica para usar funciones o metodos para agregar eliminar recorrer la cantidad de datos que esten en un lista, agregar un valor en cierto indice de la lista y otras funciones de python

--- Exercise_practicetwo.py
+++ Exercise_practicetwo.py
@@ -34,3 +34,31 @@ list_names[2] = "software"
 print(f'{list_names[2]}')
 miscellaneous_list[2] = 3034
 print(f'{miscellaneous_list[2]}')
+print(f'-----Esto es para llamar un valor de una lista-------------')
+entered_number = 33
+if entered_number in list_numbers:
+    print(f'El numero {entered_number} Encontrado')
+else:
+    print(f'El numero {entered_number} No encontrado')
+print(f'-------------Buscar un dato en una lista-----------')
+entered_number = 1991
+index_number = list_numbers.index(entered_number)
+print(f'El indice en el que se encuentra el valor {entered_number} es: {index_number}')
+#crea una sublista de la list_numbers
+sub_list = list_numbers[0:3]
+print(f'Esta es la impresion de la sublista {sub_list}')
+print(f'----------Imprimir la cantidad de datos de la lista----------')
+length_list = len(list_numbers)
+print(f'Esta la longitud de la lista {length_list}')
+#forma para agregar un dato a la lista con la funcion append
+list_numbers.append(18)
+print(f'Lista con nuevo valor {list_numbers}')
+print(f'--------Aqui estamos usando la funcion insert--------------')
+print(f'Impresion de la lista sin agregar dato {list_numbers}')
+list_numbers.insert(2,3034)
+print(f'Lista agregando valor en el indice {list_numbers}')
+length_list = len(list_numbers)
+print(f'Esta la longitud de la lista {length_list}')
+#Usando la funcion o metodo remove para eliminar un valor de la lista
+list_numbers.remove(3034)
+print(f'lista despues de eliminar el valor agregado antes 3034: {list_numbers}')
